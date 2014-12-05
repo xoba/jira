a66b3f61c266b59974d495b70e70fbe5ceddaaf5
mike andrews <mra@xoba.com>
2014-12-05 09:17:29 -0500

    get rid of extraneous file refs

--- clean.sh
+++ clean.sh
@@ -5,9 +5,7 @@ go clean -i
 find . -name "*~" -exec rm \{} \; 
 rm -rf bin
 rm -rf pkg
-find . -name "*.class" -exec rm \{} \;
 find . -name "*.test" -exec rm \{} \;
-find . -name "*.java.orig" -exec rm \{} \;
 find . -name "*flymake_*.go" -exec rm \{} \;
 find . -name "y.output" -exec rm \{} \;
 rm -rf build
--- lib/sourcefiles.sh
+++ lib/sourcefiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export GO=`go run lib/files.go src/xoba | xargs`
-echo README.md lib/*.sh *.sh *.java $GO
+echo README.md lib/*.sh *.sh $GO
 
 
